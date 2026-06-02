782d57283685b8cb6fa58ee5fb6f634d11367e66
G-Krushna <gkp.achary@gmail.com>
2026-06-03 01:12:23 +0530

    Implement Database singleton with connect function

---
+++ q8.kt
@@ -0,0 +1,13 @@
+/* 8. Singleton: Write an object Database with a fun connect() that prints "Connected to DB". */
+
+object Database {
+
+    fun connect() {
+        println("Connected to DB")
+    }
+}
+
+fun main() {
+
+    Database.connect()
+}
