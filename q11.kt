996c3b90dd863927d50d55ac9671257838b2e85e
G-Krushna <gkp.achary@gmail.com>
2026-06-03 01:18:46 +0530

    Add lambda function to calculate square of a number

---
+++ q11.kt
@@ -0,0 +1,11 @@
+/* 11. Lambda Syntax: Write a variable val square: (Int) -> Int that holds a lambda function. Use 
+the it keyword to return the square of the input number. */
+
+fun main() {
+
+    val square: (Int) -> Int = {
+        it * it
+    }
+
+    println(square(5))
+}
