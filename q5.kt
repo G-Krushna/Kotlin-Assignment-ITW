46e66e7b518baf7ea5684b753860e735efdc036c
G-Krushna <gkp.achary@gmail.com>
2026-06-03 01:08:11 +0530

    Implement abstract class Animal and Cat subclass

---
+++ q5.kt
@@ -0,0 +1,21 @@
+/* 5. Abstraction: Write an abstract class Animal with an abstract method makeSound(). 
+Write a Cat class that inherits from it and implements the sound. */
+
+abstract class Animal {
+
+    abstract fun makeSound()
+}
+
+class Cat : Animal() {
+
+    override fun makeSound() {
+        println("Meow")
+    }
+}
+
+fun main() {
+
+    val cat = Cat()
+
+    cat.makeSound()
+}
